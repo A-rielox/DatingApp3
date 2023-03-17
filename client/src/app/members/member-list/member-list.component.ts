b71d2d7ff97d785481c58cc7103bc2233d2f0584
A-rielox <arielox.ag@gmail.com>
2023-03-17 11:47:18 -0600

    lista seccion 13

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -4,7 +4,6 @@ import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
 import { User } from 'src/app/_models/user';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -16,24 +15,13 @@ export class MemberListComponent implements OnInit {
    members: Member[] = [];
    pagination: Pagination | undefined;
    userParams: UserParams | undefined;
-   user: User | undefined;
    genderList = [
       { value: 'male', display: 'Males' },
       { value: 'female', display: 'Females' },
    ];
 
-   constructor(
-      private memberService: MembersService,
-      private accountService: AccountService
-   ) {
-      this.accountService.currentUser$.pipe(take(1)).subscribe({
-         next: (user) => {
-            if (user) {
-               this.userParams = new UserParams(user);
-               this.user = user;
-            }
-         },
-      });
+   constructor(private memberService: MembersService) {
+      this.userParams = this.memberService.getUserParams();
    }
 
    ngOnInit(): void {
@@ -41,34 +29,33 @@ export class MemberListComponent implements OnInit {
    }
 
    loadMembers() {
-      if (!this.userParams) return;
+      if (this.userParams) {
+         this.memberService.setUserParams(this.userParams);
 
-      this.memberService.getMembers(this.userParams).subscribe({
-         next: (paginatedRes) => {
-            if (paginatedRes.result && paginatedRes.pagination) {
-               this.members = paginatedRes.result;
-               this.pagination = paginatedRes.pagination;
-            }
-         },
-      });
-   }
-
-   resetFilters() {
-      if (this.user) {
-         this.userParams = new UserParams(this.user);
-         this.loadMembers();
+         this.memberService.getMembers(this.userParams).subscribe({
+            next: (paginatedRes) => {
+               if (paginatedRes.result && paginatedRes.pagination) {
+                  this.members = paginatedRes.result;
+                  this.pagination = paginatedRes.pagination;
+               }
+            },
+         });
       }
    }
 
+   resetFilters() {
+      this.userParams = this.memberService.resetUserParams();
+
+      this.loadMembers();
+   }
+
    pageChanged(e: any) {
-      // if (this.userParams && this.userParams?.pageNumber !== e.page) {
-      //    this.userParams.pageNumber = e.page;
-
-      // para que tambien cambie en el memberService q es donde esta "respaldado" p' el caso en q se cambia de pagina
-      // this.memberService.setUserParams(this.userParams);
-
       if (this.userParams && this.userParams?.pageNumber !== e.page) {
          this.userParams.pageNumber = e.page;
+
+         // para que tambien cambie en el memberService q es donde esta "respaldado" p' el caso en q se cambia de pagina
+         this.memberService.setUserParams(this.userParams);
+
          this.loadMembers();
       }
    }
