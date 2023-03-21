3d54495e79c520db3194ecb48b11c5bd45fa3b04
A-rielox <arielox.ag@gmail.com>
2023-03-21 14:27:55 -0600

    listo message thread en front

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,12 +1,12 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, of, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { PaginatedResult } from '../_models/pagination';
 import { User } from '../_models/user';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
    providedIn: 'root',
@@ -85,7 +85,7 @@ export class MembersService {
 
       if (response) return of(response);
 
-      let params = this.getPaginationHeaders(
+      let params = getPaginationHeaders(
          userParams.pageNumber,
          userParams.pageSize
       );
@@ -95,9 +95,10 @@ export class MembersService {
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
 
-      return this.getPaginatedResult<Member[]>(
+      return getPaginatedResult<Member[]>(
          this.baseUrl + 'users',
-         params
+         params,
+         this.http
       ).pipe(
          map((res) => {
             this.memberCache.set(Object.values(userParams).join('-'), res);
@@ -115,43 +116,15 @@ export class MembersService {
    }
 
    getLikes(predicate: string, pageNumber: number, pageSize: number) {
-      let params = this.getPaginationHeaders(pageNumber, pageSize);
+      let params = getPaginationHeaders(pageNumber, pageSize);
 
       params = params.append('predicate', predicate);
 
-      return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
-   }
-
-   //////////////////////////////////////
-   //////////     PAGINATIONS
-   //////////////////////////////////////
-   private getPaginatedResult<T>(url: string, params: HttpParams) {
-      const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
-
-      return this.http.get<T>(url, { observe: 'response', params }).pipe(
-         map((res) => {
-            if (res.body) {
-               paginatedResult.result = res.body;
-            }
-
-            const pagination = res.headers.get('Pagination');
-
-            if (pagination) {
-               paginatedResult.pagination = JSON.parse(pagination);
-            }
-
-            return paginatedResult;
-         })
+      return getPaginatedResult<Member[]>(
+         this.baseUrl + 'likes',
+         params,
+         this.http
       );
-   }
-
-   private getPaginationHeaders(pageNumber: number, pageSize: number) {
-      let params = new HttpParams();
-
-      params = params.append('pageNumber', pageNumber);
-      params = params.append('pageSize', pageSize);
-
-      return params;
    }
 
    //////////////////////////////////////
@@ -170,4 +143,36 @@ export class MembersService {
          {}
       );
    }
+
+   //////////////////////////////////////
+   //////////     PAGINATIONS
+   //////////////////////////////////////
+   // private getPaginatedResult<T>(url: string, params: HttpParams) {
+   //    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
+
+   //    return this.http.get<T>(url, { observe: 'response', params }).pipe(
+   //       map((res) => {
+   //          if (res.body) {
+   //             paginatedResult.result = res.body;
+   //          }
+
+   //          const pagination = res.headers.get('Pagination');
+
+   //          if (pagination) {
+   //             paginatedResult.pagination = JSON.parse(pagination);
+   //          }
+
+   //          return paginatedResult;
+   //       })
+   //    );
+   // }
+
+   // private getPaginationHeaders(pageNumber: number, pageSize: number) {
+   //    let params = new HttpParams();
+
+   //    params = params.append('pageNumber', pageNumber);
+   //    params = params.append('pageSize', pageSize);
+
+   //    return params;
+   // }
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
    selector: 'app-messages',
@@ -6,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
-   constructor() {}
+   messages?: Message[];
+   pagination?: Pagination;
+   container = 'Unread';
+   pageNumber = 1;
+   pageSize = 5;
 
-   ngOnInit(): void {}
+   loading = false;
+
+   constructor(private messageService: MessageService) {}
+
+   ngOnInit(): void {
+      this.loadMessages();
+   }
+
+   loadMessages() {
+      this.messageService
+         .getMessages(this.pageNumber, this.pageSize, this.container)
+         .subscribe({
+            next: (res) => {
+               this.messages = res.result;
+               this.pagination = res.pagination;
+            },
+         });
+   }
+
+   deleteMessage(id: number) {}
+
+   pageChanged(e: any) {
+      if (this.pageNumber !== e.page) {
+         this.pageNumber = e.page;
+
+         this.loadMessages();
+      }
+   }
 }
