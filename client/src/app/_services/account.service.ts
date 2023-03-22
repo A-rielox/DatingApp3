947694596e764d8cc993546a16e4be837f89f6bd
A-rielox <arielox.ag@gmail.com>
2023-03-22 17:42:23 -0600

    listo servicio y directiva en front + show

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -14,11 +14,6 @@ export class AccountService {
 
    constructor(private http: HttpClient) {}
 
-   setCurrentUser(user: User) {
-      this.currentUserSource.next(user);
-      localStorage.setItem('user', JSON.stringify(user));
-   }
-
    login(model: any) {
       return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
          map((res) => {
@@ -46,5 +41,18 @@ export class AccountService {
                }
             })
          );
+   }
+
+   setCurrentUser(user: User) {
+      user.roles = [];
+      const roles = this.getDecodedToken(user.token).role;
+      Array.isArray(roles) ? (user.roles = roles) : user.roles.push(roles);
+
+      localStorage.setItem('user', JSON.stringify(user));
+      this.currentUserSource.next(user);
+   }
+
+   getDecodedToken(token: string) {
+      return JSON.parse(atob(token.split('.')[1]));
    }
 }
