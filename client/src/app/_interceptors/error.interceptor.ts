06e9fa8f796129f4e63f76b692b546d4403b8787
A-rielox <arielox.ag@gmail.com>
2023-03-23 16:15:04 -0600

    listo admin en front

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -73,7 +73,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                      break;
                }
             }
-
+            
             // return throwError(error);
             throw error;
          })
